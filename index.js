59d10a55d3a33abd11518c3b29e911cdfab21bce
torealdemir <aldemirtore@gmail.com>
2024-11-24 19:28:14 +0300

    feat: specific user exercise get log microservice created

--- index.js
+++ index.js
@@ -67,6 +67,46 @@ app.post('/api/users/:_id/exercises', (req, res)=>{
 });
 
 
+// 4. Get a users exercise log
+
+app.get('/api/users/:_id/logs', (req, res)=>{
+  const {_id } = req.params;
+  const { from, to, limit } = req.query;
+
+  const user = users.find((u)=> u._id = _id);
+  if(!user) {
+    return res.status(404).json({error: "User not found!"});
+  }
+
+  let userExercises = exercises.filter((e)=> e._id === _id);
+
+  if(from) {
+    const fromDate = new Date(from);
+    userExercises = userExercises.filter((e)=> new Date(e.date)>= fromDate);
+  }
+
+  if(to) {
+    const toDate = new Date(to);
+    userExercises = userExercises.filter((e) => new Date(e.date) <= toDate);
+  }
+
+  if(limit) {
+    userExercises = userExercises.slice(0, parseInt(limit));
+  }
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    count: userExercises.length,
+    log: userExercises.map((e)=> ({
+      description:e.description,
+      duration:e.duration,
+      date: e.date,
+    }))
+  })
+})
+
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
