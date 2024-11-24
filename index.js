3afeadd767ffb9f3d4714af7937279f6d04e215d
torealdemir <aldemirtore@gmail.com>
2024-11-24 19:02:46 +0300

    initial commit and feat: post request added

--- index.js
+++ index.js
@@ -5,12 +5,30 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+//In memory- stage
+
+const users = [];
+const exercises = [];
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+// 1. Create a new user
+app.post('/api/users', (req, res)=>{
+  const { username } = req.body;
+  const userId = users.length + 1; // simple incremental ID
 
+  const newUser = { _id: userId.toString(), username };
+  users.push(newUser);
+
+  res.json(newUser);
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
