a43c1a7f1c5c7c8002272a59f64c5117338e2ace
torealdemir <aldemirtore@gmail.com>
2024-11-24 19:16:20 +0300

    feat: adding exercise to a user function

--- index.js
+++ index.js
@@ -36,6 +36,37 @@ app.get('/api/users', (req, res)=> {
   res.json(users);
 })
 
+
+// 3. Add an exercise to a user
+app.post('/api/users/:_id/exercises', (req, res)=>{
+  const { _id } = req.params;
+  const { description, duration, date } = req.body;
+
+  const user = users.find((u)=> u._id === _id);
+  if(!user) {
+    return res.status(404).json({error:"User not found"});
+
+  }
+
+  const exercise = {
+    _id,
+    description,
+    duration: parseInt(duration),
+    date: date ? new Date(date).toDateString() : new Date().toDateString(),
+  };
+
+  exercises.push(exercise);
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date,
+  });
+});
+
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
